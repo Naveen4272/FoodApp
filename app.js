75a886b79f9152ec345d8013b3b5ec926fb9e10a
Naveen4272 <naveenkumar4272n@gmail.com>
2025-06-06 22:29:02 +0530

    ep3

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const heading=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"hello world from React👿👿")));
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const Title=()=>{
+  return(
+    <div className="title">
+      <h1>title</h1>
+    </div>
+  )
+}
+
+const Heading=()=>{
+  return(
+    <div id="heading">
+      <Title/>
+      <Title></Title>
+      {Title()}
+      {10000}
+      <h1>heading</h1>
+    </div>
+  )
+}
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<Heading/>)
